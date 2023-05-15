4e7b21c096ee8bc1d679e024533629324e61b892
qihaiqianqiu <44798852+qihaiqianqiu@users.noreply.github.com>
2023-05-15 13:59:41 +0800

    test

--- utils/visualize.py
+++ utils/visualize.py
@@ -486,7 +486,7 @@ if __name__ == "__main__":
     print("开始生成图像")
     print("绘制成交量切分套利图...")
     try:
-        plot_volume_split()
+        #plot_volume_split()
         print("成交量切分套利图生成完成（barplot目录）")
         print("绘制品种连续合约套利图...")
         plot_continuous_contract()
