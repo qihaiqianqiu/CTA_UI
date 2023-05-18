606a521e32d8396f27a3ae794f9f127977f3ed59
qihaiqianqiu <haohan19971114@gmail.com>
2023-05-18 13:48:46 +0800

    增加了检查参数表功能

--- utils/rename.py
+++ utils/rename.py
@@ -1,11 +1,11 @@
 import re
-from . import const
+from utils.const import breed_dict
 all = ["rename"]
-# Contract name from upper case and %ccdddd
+# Contract name from upper case and %ccdddd [params.csv] to lower case and %cdddd [breed_dict] 
 def rename(contract:str):
     code = re.search("[a-zA-Z]+", contract).group(0)
     figure = re.search("[0-9]+", contract).group(0)
-    code_mapping = const.breed_dict
+    code_mapping = breed_dict
     counter = 0
     for key, values in code_mapping.items():
         if values == code:
@@ -17,3 +17,16 @@ def rename(contract:str):
         code = code.lower()
     
     return code + figure
+
+# reverse contract string from database format into param format
+def rename_db_to_param(contract:str):
+    code = re.search("[a-zA-Z]+", contract).group(0)
+    figure = re.search("[0-9]+", contract).group(0)
+    code = code.upper()
+    if len(figure) == 3:
+        figure = "2" + figure
+        
+    return code + figure
+
+if __name__ == "__main__":
+    print(rename_db_to_param(""))
