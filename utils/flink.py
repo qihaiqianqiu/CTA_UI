af16dd46ce453d88c875fe04e146c2a063cbef50
qihaiqianqiu <haohan19971114@gmail.com>
2023-07-14 17:26:22 +0800

    重构了文件链路设计

--- utils/flink.py
+++ utils/flink.py
@@ -1,27 +1,24 @@
-import paramiko
-import os
-import sftp
 import json
-import time
-import asyncio
-import traceback
-import datetime
+from . import sftp
+from . import path_exp_switch
+import os
+import pandas as pd
+from utils.const import ROOT_PATH, INFO_PATH
+from utils.path_exp_switch import windows_to_linux
 
+__all__ = ["pull_from_UI_to_cloud", "pull_from_UI_to_market"]
+
+
+"""
+Relevant Terminal
 SSH_reverse_tunnel = "ssh -R 9876:localhost:22 root@39.97.106.35"
 
 rsync_example = "rsync -avPz --port 8730 --password-file=/cygdrive/C/Users/Han.Hao/AppData/Local/cwrsync/bin/cta_password.txt root@39.97.106.35::cta/ /cygdrive/C/Users/Han.Hao/test"
 # "rsync_pwd_path" & "rsync dest path"
-
-def windows_to_linux(path):
-    return path.replace("\\", "/")
-
-def linux_to_windows(path):
-    return path.replace("/", "\\")
-
+"""
 # 以下部署在行情服务器上 CTA目录下【同步自云服务器】
 # 行情服务器的方法挂载在后台实时运行
 # 获取根目录下的所有config并推送参数表
-        
 def set_up_ssh_reverse_tunnel(config_file):
     ftp_config = json.load(open(os.path.join(".\sftp_configs", config_file)))
     cloud_server_para = ftp_config["cloudServer"]
@@ -52,7 +49,7 @@ def pull_from_market_to_trading(config_file):
         print("行情服务器参数表成功上传至交易端", trade_param_dir)    
     ssh.close() 
     
-def request_from_market_cloud(config_file):
+def request_from_market_to_cloud(config_file):
     """行情服务器从云服务器获取最新的参数表与链路信息"""
     # 根目录 行情服务器的CTA文件夹
     ftp_config = json.load(open(os.path.join(".\sftp_configs", config_file)))
@@ -74,6 +71,12 @@ def request_from_market_cloud(config_file):
         ssh.download(windows_to_linux(os.path.join(acc_cloud_dir, "params.csv")), os.path.join(acc_dest_dir, "params.csv"))
         print("云服务器参数表成功下载至行情端", acc_dest_dir)
     ssh.close()    
+
+def request_from_trade_to_market(config_file):
+    pass
+
+def pull_from_cloud_to_market(config_file):
+    pass
 
 """
 async def config_task(config):
